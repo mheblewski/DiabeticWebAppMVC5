f18f91cb29994df05480ce9ffde9cedaeb7c05e0
mheblewski <heblewski.mateusz@gmail.com>
2018-02-25 16:48:44 +0100

    Add custom css style

--- DiabeticWebAppMVC5/App_Start/BundleConfig.cs
+++ DiabeticWebAppMVC5/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace DiabeticWebAppMVC5
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/MyStyle.css"
+                ));
         }
     }
 }
